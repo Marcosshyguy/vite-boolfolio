8ca70b00858ad8b6e666aa19e4c62a61bcff772d
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2023-01-22 21:58:50 +0100

    Add:buttons bar under grid projects

---
+++ src/components/AppGridButtons.vue
@@ -0,0 +1,59 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+  components: {},
+  props: {
+    currentPage: Number,
+    totalPages: Number,
+    lastPage: Number,
+  },
+  methods: {},
+  created() {},
+};
+</script>
+
+<template>
+  <div class="d-flex justify-content-center mt-4">
+    <!-- to get the previus or the next pages need to use the same get action in getProjects and modify the currentPage variable
+    and then put a disabled attribute if there are no more pages to display   -->
+    <div>
+      <a
+        @click.prevent="$emit('moveProject', currentPage === 1)"
+        :disabled="currentPage === 1"
+        class="btn btn-primary d-inline-block"
+        :class="currentPage === 1 ? 'disabled' : ''"
+      >
+        <i class="fa-solid fa-angles-left"></i>
+      </a>
+      <a
+        @click.prevent="$emit('moveProject', currentPage - 1)"
+        :disabled="currentPage === 1"
+        class="btn btn-primary d-inline-block mx-2"
+        :class="currentPage === 1 ? 'disabled' : ''"
+      >
+        <i class="fa-solid fa-angle-left"></i>
+      </a>
+      <span>Pagina {{ currentPage }} di {{ lastPage }} </span>
+      <a
+        @click.prevent="$emit('moveProject', currentPage + 1)"
+        :disabled="lastPage"
+        class="btn btn-primary d-inline-block mx-2"
+        :class="{ disabled: currentPage === lastPage }"
+      >
+        <i class="fa-solid fa-angle-right"></i>
+      </a>
+      <a
+        @click.prevent="$emit('moveProject', lastPage)"
+        :disabled="lastPage"
+        class="btn btn-primary d-inline-block"
+        :class="currentPage === lastPage ? 'disabled' : ''"
+      >
+        <i class="fa-solid fa-angles-right"></i>
+      </a>
+    </div>
+  </div>
+</template>
+
+<style lang="scss"></style>
--- src/components/AppProjectGrid.vue
+++ src/components/AppProjectGrid.vue
@@ -1,25 +1,44 @@
 <script>
 import axios from "axios";
 import AppProjectCard from "./AppProjectCard.vue";
+import AppGridButtons from "./AppGridButtons.vue";
 export default {
-  // name: AppProjectGrid,
+  name: "AppProjectGrid",
   data() {
-    return { apiUrl: "http://127.0.0.1:8000", projects: [] };
+    return {
+      apiUrl: "http://127.0.0.1:8000",
+      projects: [],
+      currentPage: 1,
+      lastPage: null,
+      totalPages: null,
+    };
   },
   components: {
     AppProjectCard,
+    AppGridButtons,
   },
   props: {},
   created() {
-    this.getProjects();
+    this.getProjects(this.currentPage);
   },
   methods: {
-    getProjects() {
-      axios.get(`${this.apiUrl}/api/call`).then((response) => {
-        this.projects = response.data.results;
-      });
+    getProjects(page) {
+      axios
+        .get(`http://127.0.0.1:8000/api/call`, {
+          params: {
+            page,
+          },
+        })
+        .then((response) => {
+          this.projects = response.data.results.data;
+          this.currentPage = response.data.results.current_page;
+          this.lastPage = response.data.results.last_page;
+          this.totalPages = response.data.results.total;
+          console.log(this.currentPage, this.totalPage, this.lastPage);
+        });
     },
   },
+  computed: {},
 };
 </script>
 
@@ -30,6 +49,12 @@ export default {
         <AppProjectCard :element="project" />
       </div>
     </div>
+    <AppGridButtons
+      :currentPage="currentPage"
+      :lastPage="lastPage"
+      :totalPages="totalPages"
+      @moveProject="getProjects"
+    />
   </div>
 </template>
 
